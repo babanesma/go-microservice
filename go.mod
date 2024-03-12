02a9e4b8a78f57a48304dc06c9607fb39c471925
Adham Saad <adham.sa3d@gmail.com>
2024-03-12 23:30:39 +0400

    use gorilla/mux

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module go-microservice
 
 go 1.22.1
+
+require github.com/gorilla/mux v1.8.1 // indirect
--- main.go
+++ main.go
@@ -8,14 +8,25 @@ import (
 	"os"
 	"os/signal"
 	"time"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
 	l := log.New(os.Stdout, "product-api", log.LstdFlags)
 	ph := handlers.NewProducts(l)
 
-	sm := http.NewServeMux()
-	sm.Handle("/", ph)
+	sm := mux.NewRouter()
+	getRouter := sm.Methods("GET").Subrouter()
+	getRouter.HandleFunc("/", ph.GetProducts)
+
+	putRouter := sm.Methods(http.MethodPut).Subrouter()
+	putRouter.HandleFunc("/{id:[0-9]+}", ph.UpdateProducts)
+	putRouter.Use(ph.MiddlewareValidateProduct)
+
+	addRouter := sm.Methods(http.MethodPost).Subrouter()
+	addRouter.HandleFunc("/", ph.AddProduct)
+	addRouter.Use(ph.MiddlewareValidateProduct)
 
 	s := &http.Server{
 		Addr:         ":9090",
